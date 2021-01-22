0c50dd501c4ba413532c70cfeb90a9c15846c4ea
nanpipat.k@gmail.com <nanpipat.k@gmail.com>
2021-01-22 09:18:55 +0700

    change config

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   Modal.setAppElement('#root');
 
   const objectDetectionService = nvision.objectDetection({
-    apiKey: "cdb29f355cb4059995e05420dc8d963f657898bf3a5f2f5e7a88c58279f5e4a0a1c4c4cf874594b42e413fc45c425425ac"
+    //API KEY
   });
 
   const openModal = () => {
